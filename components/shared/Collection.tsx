41d2dace291bb5c83ad99f7a94d8ecffd4fd0783
Jamie Reardon <jamie.reardon91@gmail.com>
2024-05-31 16:42:26 +0100

    update key value

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -51,7 +51,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map(image => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={`${image._id}`} />
           ))}
         </ul>
       ) : (
